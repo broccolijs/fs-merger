be13c71325bccd65f3a6a8d2df6b78ce3e8b2725
sparshithNR <nairbali@usc.edu>
2019-06-08 08:07:54 -0700

    Fixes issues related to meta data to help the funnel feature

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 "use strict";
 const fs = require('fs-extra');
 const walkSync = require('walk-sync');
-
+const path = require('path');
 class FSMerge {
   constructor(paths) {
     this._dirList = Array.isArray(paths) ? paths : [paths];
@@ -11,7 +11,13 @@ class FSMerge {
     let { _dirList } = this;
     let result = null;
     for (let i=0; i < _dirList.length; i++) {
-      let fullPath = _dirList[i] + '/' + filePath;
+      let basePath = '';
+      if (typeof _dirList[i] == 'string') {
+        basePath = _dirList[i];
+      } else {
+        basePath = _dirList[i].root;
+      }
+      let fullPath = basePath + '/' + filePath;
       if(fs.existsSync(fullPath)) {
         result = fs.readFileSync(fullPath, options);
       }
@@ -19,22 +25,40 @@ class FSMerge {
     return result;
   }
 
-  readFilePath(filePath, options) {
+  readFileMeta(filePath, options) {
     let { _dirList } = this;
     let result = null;
+    let { basePath } = options;
     for (let i=0; i < _dirList.length; i++) {
-      let fullPath = _dirList[i] + '/' + filePath;
+      let root = _dirList[i].root ? _dirList[i].root : _dirList[i];
+      if (basePath == root) {
+        return {
+          path: root + '/' + filePath,
+          prefix: _dirList[i].prefix || ''
+        }
+      }
+      let fullPath = root + '/' + filePath;
       if(fs.existsSync(fullPath)) {
-        result = fullPath;
+        result = {
+          path: fullPath,
+          prefix: _dirList[i].prefix || ''
+        };
       }
     }
     return result;
   }
+
   readDirSync(dirPath, options) {
     let { _dirList } = this;
     let result = [];
     for (let i=0; i < _dirList.length; i++) {
-      let fullDirPath = _dirList[i] + '/' + dirPath;
+      let basePath = '';
+      if (typeof _dirList[i] == 'string') {
+        basePath = _dirList[i];
+      } else {
+        basePath = _dirList[i].root;
+      }
+      let fullDirPath = basePath + '/' + dirPath;
       fullDirPath = fullDirPath.replace(/(\/|\/\/)$/, '');
       if(fs.existsSync(fullDirPath)) {
         result.push.apply(result, fs.readdirSync(fullDirPath, options));
@@ -46,21 +70,29 @@ class FSMerge {
   entries(dirPath = '', options) {
     let { _dirList } = this;
     let result = [];
+    let hashStore = {};
     for (let i=0; i < _dirList.length; i++) {
-      let fullDirPath = dirPath ? _dirList[i] + '/' + dirPath : _dirList[i];
+      let basePath = '';
+      let prefix = '';
+      if (typeof _dirList[i] == 'string') {
+        basePath = _dirList[i];
+      } else {
+        basePath = _dirList[i].root;
+        prefix = _dirList[i].prefix;
+      }
+      let fullDirPath = dirPath ? basePath + '/' + dirPath : basePath;
       fullDirPath = fullDirPath.replace(/(\/|\/\/)$/, '');
       if(fs.existsSync(fullDirPath)) {
         let curEntryList = walkSync.entries(fullDirPath, options);
-        result.push.apply(result, curEntryList);
+        hashStore = curEntryList.reduce((hashStoreAccumulated, entry) => {
+          hashStoreAccumulated[path.join(entry.relativePath, prefix)] = entry;
+          return hashStoreAccumulated;
+        }, hashStore);
       }
     }
+    result = Object.values(hashStore);
     result.sort((entryA, entryB) => (entryA.relativePath > entryB.relativePath) ? 1 : -1);
-    let hashStore = result.reduce((hashStoreAccumulated, entry) => {
-      hashStoreAccumulated[entry.relativePath] = entry;
-      return hashStoreAccumulated;
-    }, {});
-
-    return Object.values(hashStore);
+    return result;
   }
 }
 
